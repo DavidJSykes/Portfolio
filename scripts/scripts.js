77cfad5764f462e30ce595b008b1b232cbedac6e
David Sykes <davidjsykes@gmail.com>
2020-04-30 14:06:01 +0100

    adjusted clocked to always display MM:SS if under 10 for either

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,7 +5,7 @@ var showCurrentTime = function()
     let hours = currentTime.getHours();
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
-    let clockTime = `${hours > 12 ? hours - 12 : hours}:${minutes}:${seconds} ${hours > 12 ? 'PM' : 'AM'}`;
+    let clockTime = `${hours > 12 ? hours - 12 : hours}:${minutes > 10 ? minutes : '0' + minutes}:${seconds > 10 ? seconds : '0' + seconds} ${hours > 12 ? 'PM' : 'AM'}`;
     clock.innerText = clockTime;
 };
 showCurrentTime();
